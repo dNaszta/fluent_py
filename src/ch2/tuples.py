13c3719961ecea83e47428978f9599f52efac301
dnaszta <david.nasztanovics@stylersonline.com>
2018-06-26 09:49:50 +0200

    add logic

--- src/ch2/tuples.py
+++ src/ch2/tuples.py
@@ -1,4 +1,5 @@
 import os
+from collections import namedtuple
 
 lax_coordinates = (33.9425, -118.408056)
 latitude, longitude = lax_coordinates
@@ -16,4 +17,38 @@ print(quotient, remainder)
 _, dirname = os.path.split('/var/log/ize.md')
 print(dirname)
 
-#29
+a, b, *rest = range(5)
+print(a, b, rest)
+
+a, *body, c, d = range(5)
+print(a, body, c, d)
+
+
+metro_areas = [
+    ('Tokyo', 'JP', 36.933, (35.689722, 139.691667)),
+    ('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889)),
+    ('Mexico City', 'MX', 20.142, (19.433333, -99.133333)),
+    ('New York-Newark', 'US', 20.104, (40.808611, -74.020386)),
+    ('Sao Paulo', 'BR', 19.649, (-23.547778, -46.635833)),
+]
+
+print('{:15} | {:^9} | {:^9}'.format('', 'lat.', 'long.'))
+fmt = '{:15} | {:9.4f} | {:9.4f}'
+for name, cc, pop, (latitude, longitude) in metro_areas:
+    if longitude <= 0:
+        print(fmt.format(name, latitude, longitude))
+print()
+
+City = namedtuple('City', 'name country population coordinates')
+tokyo = City('Tokyo', 'JP', 36.933, (35.689722, 139.691667))
+print(tokyo)
+print(tokyo.population, tokyo.coordinates, tokyo[1])
+
+print(City._fields)
+LatLong = namedtuple('LatLong', 'lat long')
+delhi_data = ('Delhi NCR', 'IN', 21.935, LatLong(28.613889, 77.208889))
+delhi = City._make(delhi_data)
+print(delhi._asdict())
+
+for key, value in delhi._asdict().items():
+    print(key + ':', value)
