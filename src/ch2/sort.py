3b30b2f528c756ac2e71f6f11b67be327ab80d1d
dnaszta <david.nasztanovics@stylersonline.com>
2018-06-26 16:57:25 +0200

    some stuff

---
+++ src/ch2/sort.py
@@ -0,0 +1,8 @@
+fruits = ['grape', 'raspberry', 'apple', 'banana']
+print(fruits)
+print(sorted(fruits))
+print(sorted(fruits, reverse=True))
+print(sorted(fruits, key=len))
+print(sorted(fruits, key=len, reverse=True))
+fruits.sort()
+print(fruits)
